4c1e1f4282a7129f95299bd77bca802040552585
unknown <Dan@.(none)>
2010-12-11 08:40:07 -0800

    Initial commit of Drupal Power Admin Bookmarklets.

---
+++ build.php
@@ -0,0 +1,299 @@
+<?php
+
+
+$bookmarklet_tree =
+  array('Xperimental Bookmarklets', array(
+
+    array('Edit this user',   'user/{UID_FROM_EDIT}/edit'),
+    array('This user\'s order history',   'user/{UID_FROM_EDIT}/order-history'),
+
+    array('V - View', array(
+      array('This node', 'node/{NID_FROM_EDIT}'),
+      array('Node #',    'node/{PROMPT:Enter node # (nid)}'),
+      array('User #',    'user/{PROMPT:Enter user # (uid)}'),
+    )),
+    array('E - Edit', array(
+      array('This node', 'node/{NID_FROM_EDIT}/edit'),
+      array('Node #',    'node/{PROMPT:Enter node # (nid)}/edit'),
+      array('User #',    'user/{PROMPT:Enter user # (uid)}/edit'),
+    )),
+    array('D - Delete', array(
+      array('This node', 'node/{NID_FROM_EDIT}/delete'),
+      array('Node #',    'node/{PROMPT:Enter node # (nid)}/delete'),
+      array('User #',    'user/{PROMPT:Enter user # (uid)}/delete'),
+    )),
+    array('-------------------------'),
+    array('z - Menu',  'admin/build/menu'),
+    array('W - Views', 'admin/build/views'),
+    array('P - Permissions', 'admin/user/permissions'),
+    array('Q - mysQL (phpmyadmin) (TO DO)',          'to_do'),
+    array('1 - HTTP (switch to non-secure) (TO DO)', 'to_do'),
+    array('2 - HTTPS (switch to secure) (TO DO)',    'to_do'),
+    array('A - Add', array(
+      array('L - List', 'node/add'),
+      array('P - Page', 'node/add/page'),
+      array('N - Node (node/add/brew_your_own)',   'node/add/{PROMPT:Node type to create?}'),
+      array('T - Type (create a new content type)', 'admin/content/types/add'),
+      array('U - User', 'admin/user/user/create'),
+    )),
+    array('L - List', array(
+      array('C - Content', 'admin/content/node'),
+      array('F - Fields',  'admin/content/types/fields'),
+      array('T - Types',   'admin/content/types'),
+      array('U - Users',   'admin/user/user'),
+    )),
+    array('-------------------------------'),
+    array('N - logiN',  'user/login'),
+    array('T - logouT', 'logout'),
+    array('-------------------------------'),
+    array('K - blocK',    'admin/build/block'),
+    array('M - Modules',  'admin/build/modules'),
+    array('H - tHemes',   'admin/build/themes'),
+    array('X - taXonomy', 'admin/content/taxonomy'),
+    array('G - watchdoG', 'admin/reports/dblog'),
+    array('-------------------------------'),
+    array('Y - bYo - brew Your own',   '{PROMPT:Enter your Drupal URL suffix}'),
+    array('-------------------------------'),
+    array('X - Server Switching', array(
+      array('b - BCEOHRN',        '>>>> bceohrn.ca'),
+      array('s - BCEOHRN search', '>>>> http://bceohrn.ca/search'),   // Why not just leave out support for this for now?  Who besides Joe puts a drupal install insides another drupal install??
+      array('FWWD', array(
+        array('live',  '>>>> dev.fwwd:8080/ERdecisions.com'),
+        array('stage', '>>>> dev.fwwd:8080/ERdecisions.staging'),
+        array('dev',   '>>>> dev.fwwd:8080/ERdecisions.development'),
+      )),
+    )),
+  ));
+
+$extra_install_subdir_samples_containers = array(
+  'secret.ca/sample_site',
+  'confidential.com/drupal_installs/some_site',
+);
+pre_parse_servers(array($extra_install_subdir_samples_containers, $bookmarklet_tree));
+
+
+
+$extractives = array(
+    array(   'function'     => 'extract_values_from_linkarray',
+             'extract_into' => 'NID_FROM_EDIT',
+             'regexp'       => "\/node\/(\d+)\/edit\b",
+    ),
+    array(   'function'     => 'extract_values_from_linkarray',
+             'extract_into' => 'UID_FROM_EDIT',
+             'regexp'       => "\/user\/(\d+)\/edit\b",
+    ),
+);
+
+
+
+
+print render_tree($bookmarklet_tree);
+
+
+
+
+
+function pre_parse_servers($lists) {
+  // Recurse through arrays of arrays.
+  // Whenever anything is found starting with ">>>>", add that to list of install dirs to look for.
+  // It will infer (assume) that ">>>> someserver.com/my_site" means "someserver.com is a place to look
+  //    for drupal install in subdirs of it.
+}
+
+function render_tree($tree) {
+  return "<DL><P>\n" . render_subtree($tree) . "</DL>\n";
+}
+
+// Recursively render sub-trees.
+function render_subtree($subtree) {
+  static $depth = 1;  // keeps track of how many levels deep we are, for indenting and pretty-printing.
+  $output = '';
+  $output .= indented($depth, "<DT><H3>$subtree[0]</H3>");
+  $output .= indented($depth, "<DL><P>");
+  ++$depth;
+  foreach ($subtree[1] as $element ) {
+    //  Sanity check.
+    if (! is_string($element[0])) {
+      die("First element must be a string.");
+    }
+    //  Render horizontal-rule (line), testing for '--------', 4 chars or longer.
+    else if (preg_match("/^ *-{4,} *$/", $element[0])) {   
+      $output .= indented($depth, "<HR>");
+    }
+    //  Render sub-dir (recursion).
+    else if (is_array($element[1])) {
+      $output .= render_subtree($element);
+    }
+    // Render normal link.
+    else if (is_string($element[1])) {
+      $output .= indented($depth, "<DT><A HREF=\"javascript:". get_js($element[1]) ."\">$element[0]</A>");
+      // print indented($depth, "<DD>(comment goes here)</DD>");
+    }
+  }
+  --$depth;
+  $output .= indented($depth, "</DL><P>");
+  return $output;
+}
+
+function indented($depth, $text) {
+  return str_repeat(' ', $depth*2) . $text . "\n";
+}
+
+function get_js($url_suffix) {
+  $bookmarklet_js = '';
+  global $extractives;
+
+  foreach ($extractives as $extractive) {
+    $extractive['function']( $url_suffix, $bookmarklet_js, $extractive['extract_into'], $extractive['regexp'] );
+  }
+  
+  if (!$bookmarklet_js) {
+    get_normal_bookmarkletjs($url_suffix, $bookmarklet_js);
+  }
+  return html_escaped($bookmarklet_js);
+}
+
+
+
+function html_escaped($my_js) {
+  return str_replace(
+          array('"'),
+          array('&quot;'),
+          $my_js
+  );
+}
+
+
+
+// Convert this:   "node/{NID_FROM_EDIT}/delete"
+//    into this:   array('before_nid' => 'node/', 'after_nid' => '/delete')
+// Returns NULL if no match.
+function extract_values_from_linkarray(&$url_suffix, &$bookmarklet_js, $extract_into, $regexp) {
+  $matches = array();
+  if (preg_match("/(^.*){".$extract_into."}(.*$)/", $url_suffix, $matches)) {
+    $before_nid = $matches[1];
+    $after_nid  = $matches[2];
+    $url_suffix = '';  // because we're done processing it.
+
+    $dest_parts = array();
+    if ($before_nid) {
+      $dest_parts[] = "'$before_nid'";
+    }
+    $dest_parts[] = 'a[0]';  // This will be the extracted Node ID, if the regexp works.
+    if ($after_nid) {
+      $dest_parts[] = "'$after_nid'";
+    }
+
+    $REGEXP_FRAGMENT = $regexp;  // should be something like "\/node\/(\d+)\/edit\b"
+    $DESTINATION = implode(' + ', $dest_parts);   // should be something like:  "'node/' + a[0] + '/delete'"
+
+    $bookmarklet_js = <<<END_JS
+      lin = document.links;
+      var regexp = /(.*)$REGEXP_FRAGMENT/;
+      var a=[];
+      for (y=0; y<lin.length; y++)  {
+        if (regexp.test(lin[y].href)==true) {
+          urlroot = RegExp.$1;
+          a.push(RegExp.$2);
+        }
+      }
+      switch (a.length) {
+        case 0:
+          alert ('No node-edit link found-- unable to infer node to delete.');
+          break;
+        case 1:
+          location.href = urlroot + '/' + $DESTINATION;
+          break;
+        default:
+          alert ('Multiple node-edit links found on page-- unable to infer node to delete.');
+      }
+END_JS;
+  }
+}
+
+
+
+function get_normal_bookmarkletjs(&$url_suffix, &$bookmarklet_js) {
+
+  $bookmarklet_js = <<<END_JS
+    var regex1=/(https?:\/\/(localhost|127.0.0.1|dev.fwwd:8080|74.50.62.70:8080))[\/]?([^\/]*|)/i;
+    var regex2=/(https?:\/\/[^\/]*)[\/]?(.*drupal[^\/]*|)/i;
+    var lh=location.href;
+    if (regex1.test(lh)==true){
+      url_root=RegExp.$1;
+      install_subdir=RegExp.$3;
+    }
+    else if(regex2.test(lh)==true){
+      url_root=RegExp.$1;
+      install_subdir=RegExp.$2;
+    }
+    else{
+      alert("Couldn't extract server name from current URL, '" + lh + "' !");
+    }
+
+    if (install_subdir){
+      url_root=url_root + '/' + install_subdir;
+    }
+END_JS;
+  // A bit more JS yet to be added... still have to set location.href:
+
+  $dest_array = check_for_prompt_in_urlsuffix($url_suffix);
+  if ($dest_array) {
+    // We have to prompt the user for part of the URL.
+    //  $dest_array['urlsuffix_string_js'] will be something like:  "'node/add/' + user_response"
+    $bookmarklet_js .= sprintf(<<<END_JS
+      if(url_root){
+        user_response = prompt('%s');
+        if (user_response != null) {
+          location.href = url_root + '/' + %s;
+        }
+        else {
+          alert("Cancelled.");  /* Not sure why it fails without this line. */
+        }
+      }
+END_JS
+            , $dest_array['prompt_message']
+            , $dest_array['urlsuffix_string_js']);
+  }
+  else {
+    // The URL is prescribed.  Simple.
+    $bookmarklet_js .= sprintf(<<<END_JS
+      if(url_root){
+        location.href = url_root + '/' + '%s';
+      }
+END_JS
+            , $url_suffix);
+  }
+
+  $url_suffix = '';  // because we're done processing it.
+  return html_escaped($bookmarklet_js);
+}
+
+
+
+function check_for_prompt_in_urlsuffix($dest) {
+  $matches = array();
+
+  // Convert this:   "some/dir/{PROMPT:My message}"
+  //    into this:   destination    => "'some/dir/' + user_response"
+  //             and prompt_message => My message"
+  if (preg_match("/(^.*)({PROMPT:([^}]*)})(.*$)/", $dest, $matches)) {
+
+    $parts = array();
+    if (!empty($matches[1])) {
+      $parts[] = "'$matches[1]'";
+    }
+    $parts[] = "user_response";  // a javascript var, into which we will have stored the user-input.
+    if (!empty($matches[4])) {
+      $parts[] = "'$matches[4]'";
+    }
+    return array(
+        'prompt_message' => "$matches[3]",
+        'urlsuffix_string_js' => implode(' + ', $parts)
+    );
+  }
+  else {  // no match
+    return null;
+  }
+}
+
